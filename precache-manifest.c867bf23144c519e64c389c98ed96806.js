ee0e0f94c455e2d69f1da591d7315b3a0869c441
gamerHat <pranavkulkarni195@gmail.com>
2020-07-15 09:21:19 +0000

    Add files via upload

---
+++ precache-manifest.c867bf23144c519e64c389c98ed96806.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105d8c1afbdb7195854b473f5484fd66",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e4220f6fa77260d7ee7",
+    "url": "./static/css/main.9784aef1.chunk.css"
+  },
+  {
+    "revision": "f8fa9999408aec22b078",
+    "url": "./static/js/2.ad17785f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.ad17785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4220f6fa77260d7ee7",
+    "url": "./static/js/main.f8715f1a.chunk.js"
+  },
+  {
+    "revision": "138edcdf50cf19a90210",
+    "url": "./static/js/runtime-main.5bf6b89d.js"
+  },
+  {
+    "revision": "df09b85f65b6d2a5184545f62633853d",
+    "url": "./static/media/1.df09b85f.svg"
+  },
+  {
+    "revision": "a5ad05f8d944125c10cc352adb67034d",
+    "url": "./static/media/book5.a5ad05f8.svg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "./static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "234d17047605b3fe41ee2ad433616d99",
+    "url": "./static/media/game-controller9.234d1704.svg"
+  },
+  {
+    "revision": "263f2fb9437cd75fead88a9acc0cac04",
+    "url": "./static/media/gift.263f2fb9.svg"
+  },
+  {
+    "revision": "f5f4cf42e73088e6e222d341a971aa79",
+    "url": "./static/media/hammer5.f5f4cf42.svg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "./static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "./static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "./static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "./static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "84107fd85132e111cde476f4b4d16662",
+    "url": "./static/media/popcorn.84107fd8.svg"
+  },
+  {
+    "revision": "236835cc3073a7b0e37c9baad8a446fc",
+    "url": "./static/media/rocket.236835cc.svg"
+  },
+  {
+    "revision": "5edfca39d8fd8c138bf7b8d215ae0ce9",
+    "url": "./static/media/square-purple-compressed.5edfca39.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "./static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "./static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "./static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "./static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "./static/media/square5.ff004a0e.png"
+  }
+]);
